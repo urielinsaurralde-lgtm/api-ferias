c88669ed537a5c9ef71ede10a487e28d81702eef
Uriel Insaurralde <urielinsaurralde@gmail.com>
2026-04-14 08:58:14 -0300

    conexion a railway

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(express.json());
 
 // 🔗 conexión MySQL
 const db = mysql.createPool({
-  host: "mysql.railway.internal",
-  user: "root",
-  password: "IxlfPUfDAujbIQjAtRTgMsrpEaZMVrjb",
-  database: "railway",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10
 });
